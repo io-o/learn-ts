d61eb4bdc6f6ac71c1dadbe27b61afc26f92f3cc
io_o <>
2022-05-04 19:46:14 +0800

    fix: perfect type

--- 3.ts
+++ 3.ts
@@ -2,7 +2,7 @@
  * @Author: Andrew Q
  * @Date: 2022-05-04 19:11:45
  * @LastEditors: Andrew Q
- * @LastEditTime: 2022-05-04 19:32:59
+ * @LastEditTime: 2022-05-04 19:45:05
  * @Description: tuple2Object
  */
 
@@ -12,7 +12,7 @@ const tuple = ['model', 'model 3'] as const;
 type R = typeof tuple
 
 // type interface 是ts 语法 属于ts世界
-type Tuple2Object<T extends readonly any[]> = {
+type Tuple2Object<T extends readonly (string | number | symbol)[]> = {
   [P in T[number]]: P;
 }
 
